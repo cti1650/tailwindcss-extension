df6a2dd41d387c665fc5151a0e0e85e58ad30e2b
cti1650 <cti1650@gmail.com>
2021-09-07 04:46:01 +0900

    ✨ Pythonスクリプト追加

---
+++ scripts/update/patch.py
@@ -0,0 +1,20 @@
+import json
+from collections import OrderedDict
+
+json_url = 'extensions/manifest.json'
+
+with open(json_url) as f:
+    d_update = json.load(f, object_pairs_hook=OrderedDict)
+
+version = d_update['version'].split('.')
+
+if len(version) == 1:
+    version.extend(['0','0'])
+elif len(version) == 2:
+    version.extend(['0'])
+version[2] = str(int(version[2]) + 1)
+
+d_update['version'] = '.'.join(version)
+
+with open(json_url, 'w') as f:
+    json.dump(d_update, f, indent=2, ensure_ascii=False)
--- zip.py
+++
@@ -1,3 +0,0 @@
-import shutil
-
-print(shutil.make_archive('zip_extensions', 'zip', root_dir='./extensions'))
