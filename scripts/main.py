c563546befc0fd7892c50259531955dea91e8489
cti1650 <cti1650@gmail.com>
2021-09-12 02:12:52 +0900

    ✨ pythonのコードを見直しアイコン生成機能追加

--- scripts/main.py
+++ scripts/main.py
@@ -1,61 +1,121 @@
+from collections import OrderedDict
+import os
 import sys
 import json
 import shutil
-from collections import OrderedDict
+import glob
+import cv2
+import numpy as np
+from PIL import Image
 
 current_path = 'extensions'
 
 
-def update(key):
-    json_url = current_path + '/manifest.json'
+def manifest_data():
+    file = glob.glob('**/manifest.json')
+    if file:
+        with open(file[0]) as f:
+            d_update = json.load(f, object_pairs_hook=OrderedDict)
+        return {
+            "fullpath": file[0],
+            "path": os.path.relpath(file[0]).replace(os.path.basename(file[0]), ''),
+            "name": os.path.basename(file[0]),
+            "dirname": os.path.dirname(file[0]),
+            "json": d_update
+        }
+    else:
+        return {
+            "fullpath": '',
+            "path": '',
+            "name": '',
+            "dirname": '',
+            "json": ''
+        }
 
-    with open(json_url) as f:
+
+def load_manifest():
+    with open(manifest_data()["fullpath"]) as f:
         d_update = json.load(f, object_pairs_hook=OrderedDict)
+    return d_update
 
-    version = d_update['version'].split('.')
 
+def save_manifest(dic):
+    with open(manifest_data()["fullpath"], 'w') as f:
+        json.dump(dic, f, indent=2, ensure_ascii=False)
+
+
+def setting_version(version_str, key):
+    version = version_str.split('.')
+    if len(version) == 1:
+        version.extend(['0', '0'])
+    elif len(version) == 2:
+        version.extend(['0'])
     if key == "major":
         version[0] = str(int(version[0]) + 1)
-        if len(version) == 1:
-            version.extend(['0', '0'])
-        elif len(version) == 2:
-            version.extend(['0'])
         version[1] = str(0)
         version[2] = str(0)
-
-        d_update['version'] = '.'.join(version)
-
-        with open(json_url, 'w') as f:
-            json.dump(d_update, f, indent=2, ensure_ascii=False)
     elif key == "minor":
-        if len(version) == 1:
-            version.extend(['0', '0'])
-        elif len(version) == 2:
-            version.extend(['0'])
         version[1] = str(int(version[1]) + 1)
         version[2] = str(0)
-
-        d_update['version'] = '.'.join(version)
-
-        with open(json_url, 'w') as f:
-            json.dump(d_update, f, indent=2, ensure_ascii=False)
     elif key == "patch":
-        if len(version) == 1:
-            version.extend(['0', '0'])
-        elif len(version) == 2:
-            version.extend(['0'])
         version[2] = str(int(version[2]) + 1)
+    elif key == "release":
+        version[0] = str(1)
+        version[1] = str(0)
+        version[2] = str(0)
+    elif key == "new":
+        version[0] = str(0)
+        version[1] = str(0)
+        version[2] = str(1)
+    return '.'.join(version)
 
-        d_update['version'] = '.'.join(version)
 
-        with open(json_url, 'w') as f:
-            json.dump(d_update, f, indent=2, ensure_ascii=False)
-    else:
-        print("error")
+def update(key):
+    json = load_manifest()
+    json['version'] = setting_version(json['version'], key)
+    save_manifest(json)
 
 
 def zip():
-    print(shutil.make_archive('zip_' + current_path, 'zip', root_dir=current_path))
+    data = manifest_data()
+    print(shutil.make_archive(
+        'zip_' + data["dirname"], 'zip', root_dir=data["path"]))
+
+
+def iconResizeDataSave(img, size=128):
+    baseName = "/icons/icon"
+    if img:
+        data = manifest_data()
+        resize_img = img.resize((size, size))
+        img_path = data["path"] + baseName + "-" + \
+            str(size) + "x" + str(size) + ".png"
+        json = data["json"]
+        resize_img.save(img_path)
+        json["icons"][str(size)] = baseName + "-" + \
+            str(size) + "x" + str(size) + ".png"
+        save_manifest(json)
+
+
+def clearBkColor(img_path, img_savepath):
+    img = cv2.imread(img_path, -1)
+    img[:, :, 3] = np.where(np.all(img == 255, axis=-1),
+                            0, 255)
+    cv2.imwrite(img_savepath, img)
+
+
+def iconResize():
+    data = manifest_data()
+    baseName = "/icons/icon"
+    if os.path.isfile(data['path'] + baseName + ".png"):
+        clearBkColor(data['path'] + baseName + ".png",
+                     data['path'] + baseName + "_transparent.png")
+        icon_view = Image.open(data['path'] + baseName + "_transparent.png")
+        iconResizeDataSave(icon_view, size=128)
+        iconResizeDataSave(icon_view, size=48)
+        iconResizeDataSave(icon_view, size=19)
+        iconResizeDataSave(icon_view, size=16)
+    else:
+        print("not icon")
 
 
 if(len(sys.argv) > 1):
@@ -67,7 +127,15 @@ if(len(sys.argv) > 1):
         update(key)
     elif key == "patch":
         update(key)
+    elif key == "new":
+        update(key)
+    elif key == "release":
+        update(key)
     elif key == "zip":
         zip()
+    elif key == "mkicon":
+        iconResize()
+    elif key == "manifest":
+        print(manifest_data())
     else:
         print("error")
