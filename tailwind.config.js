83cffeaa41503e177231c3f5ebaab550efeec155
cti1650 <cti1650@gmail.com>
2021-09-19 12:34:47 +0900

    ✨ Tailwindcssの導入

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  purge: ['./pages/**/*.{js,jsx,ts,tsx}', './components/**/*.{js,jsx,ts,tsx}'],
+  mode: 'jit',
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
